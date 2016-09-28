27e4de18cfb38a50348c89ee6cc016f8f2f2be6f
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-09-27 21:50:47 -0400

    initial commit

---
+++ controllers/tasks_controller.js
@@ -0,0 +1,30 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+router.post('/:person_id/tasks/create', function (req, res) {
+  models.Task.create({
+    task: req.body.task,
+    person_id: req.params.person_id
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+
+router.delete('/people/:person_id/delete/:id', function(req,res) {
+
+  models.Task.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  // connect it to this .then.
+  .then(function() {
+    res.redirect('/');
+  });
+
+});
+
+
+module.exports = router;
